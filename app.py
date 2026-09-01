11e3404ba412c69b30231a647110ed000352a8fc
kfcc022-maker <kfcc022@synnex.kr>
2026-09-01 14:40:10 +0900

    Fix formatting of rainbow text in markdown

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.markdown(
     """ 
     This is a playground for you to try Streamlit and have fun. 
 
-    **:rainbow [There's rainbow so much you can build!]**
+    **:rainbow[There's rainbow so much you can build!]**
     
     We prepared a few examples for you to get started. Just 
     click on the buttons above and discover what you can do 
